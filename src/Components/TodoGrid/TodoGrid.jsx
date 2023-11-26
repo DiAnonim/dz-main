be6b402a03fe959a1c884e924b13f8d0316a5d72
DiAnonim <akopyan.2002@mail.ru>
2023-11-26 17:24:36 +0600

    lesson-13.3

--- src/Components/TodoGrid/TodoGrid.jsx
+++ src/Components/TodoGrid/TodoGrid.jsx
@@ -3,7 +3,6 @@ import s from "./TodoGrid.module.scss";
 import Todo from "../Todo/Todo";
 
 function TodoGrid({ todos = [] }) {
-
     return <div className={s.todoGrid}>
         {todos.map(e => (<Todo title={e.title} value={e.task} date={e.date}/>))}
         </div>
